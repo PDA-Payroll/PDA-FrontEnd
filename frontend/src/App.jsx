71c7e60e6826d6a64599aee5d02851ed9db20346
rane20 <145800950+rane20@users.noreply.github.com>
2024-04-18 23:35:16 -0700

    new frontend project utilizing react and vite. fixed vulnerabilities. utilizing tailwind

---
+++ frontend/src/App.jsx
@@ -0,0 +1,14 @@
+import Home from './Pages/home';
+import './App.css';
+import './Styles/home.css';
+
+function App() {
+  return (
+    <div className="App">
+      <Home />
+    </div>
+  );
+}
+
+export default App;
+
---
+++ frontend/tailwind.config.js
@@ -0,0 +1,12 @@
+module.exports = {
+  purge: ['./index.html', './src/**/*.{js,jsx,ts,tsx,vue}'],
+  darkMode: false,
+  theme: {
+    extend: {},
+  },
+  variants: {
+    extend: {},
+  },
+  plugins: [],
+}
+
