9f58173c6f876fa7d10efa36b23fef8731e93d71
cklindt <cklindt2003@gmail.com>
2024-05-06 13:42:46 -0700

    modifier

--- frontend/src/Pages/EmployeeModifier.jsx
+++ frontend/src/Pages/EmployeeModifier.jsx
@@ -1,12 +1,23 @@
 import '@mantine/core/styles/Modal.css';
 import axios from 'axios';
+import React from 'react';
 import '../Styles/EmployeeModifier.css'
 import {MantineProvider,Modal, Button} from "@mantine/core"
 import AdminHome from './AdminHome';
 import { useDisclosure } from '@mantine/hooks';
+import { useState } from 'react';
+import { Form } from 'react-router-dom';
 
 export default function employeeModifier(){
-
+    const [firstName, setFirstName] = useState('');
+    const [middleName, setMiddleName] = useState('');
+    const [lastName, setLastName] = useState('');
+    const [userName, setUserName] = useState('');
+    const [password, setPassword] = useState('');
+    const [isAdmin, setIsAdmin] = useState(false);
+    const [isSupervisor, setIsSupervisor] = useState(false);
+    const [ssn, setSsn] = useState('');
+    const [sId, setSId] = useState('');
     const createEmployee = () => {
         axios
         .post('http://${backendServer}/employee/post/create',{
@@ -19,16 +30,19 @@ export default function employeeModifier(){
             isSupervisor: isSupervisor,
             socialSecurityNumber: ssn,
             supervisorId: sId,
-        }
-    )
-    }
+        })
+        .then((response)=>{
+            console.log(response);
+        })
+        .catch((error)=>{
+            console.log(error);
+        });
+    };
     const [opened, {open, close}] = useDisclosure(false);
     return(
         <MantineProvider>
-            <Modal opened={opened} onClose={close} title="Create Employee Account">
-                {
-                    <Text>Me when I uh</Text>
-                }
+            <Modal opened={opened} onClose={close} title="Create Employee Account" centered>
+            
             </Modal>
             <Button style={{color: "black"}}onClick={open}>
                Create Employee 
